e1a0f49d8fbf3c52fc2952aa8135f4b40390fae4
Matouš Kozma <mattkozma@hotmail.com>
2019-11-21 17:57:02 +0100

    Added Camera centering.

---
+++ Assets/Scripts/Camera/CameraCentering.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+/// <summary>
+/// This behavior centers the camera on the player characters. Will just find their center, if they're scattered, too bad.
+/// </summary>
+public class CameraCentering : MonoBehaviour
+{
+    CombatantsManager combatantsManager;
+    private void Awake()
+    {
+        combatantsManager = FindObjectOfType<CombatantsManager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Center();
+    }
+
+    public void Center()
+    {
+        var alivePlayerCharacters = combatantsManager.GetPlayerCharacters(onlyAlive: true).ToList();
+        var playerPositions = alivePlayerCharacters.Select(character => character.transform.position);
+        var playerCenter = playerPositions.Aggregate((position1, position2) => position1 + position2) / alivePlayerCharacters.Count;
+        transform.position = new Vector3(playerCenter.x, playerCenter.y, transform.position.z);
+    }
+}
