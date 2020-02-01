25e5f3a95a7dd82b4691cda7aeb62c725025ecec
Matouš Kozma <mattkozma@hotmail.com>
2020-02-01 13:51:55 +0100

    Prepared animations and animation controllers for all monsters, though they do not have correct animations done yet.

--- Assets/Scripts/Movement/OrientationController.cs
+++ Assets/Scripts/Movement/OrientationController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class OrientationController : MonoBehaviour
 {
+    // If true, the sprite is mirrored, so our orientations should also be mirrored
+    public bool IsSpriteMirrored = false;
     public Vector2? LookAtTarget = null;
     // Update is called once per frame
     void Update()
     {
+        var transformMultiplier = IsSpriteMirrored ? -1 : 1;
         Vector2 orientationVector;
         if (LookAtTarget == null)
         {
@@ -17,10 +20,10 @@ public class OrientationController : MonoBehaviour
             Vector2 currentPosition = transform.position;
             orientationVector = LookAtTarget.Value - currentPosition;
         }
-        if ((orientationVector.x > 0 && transform.localScale.x < 0) ||
-            (orientationVector.x < 0 && transform.localScale.x > 0))
+        if ((orientationVector.x > 0 && transform.localScale.x * transformMultiplier < 0) ||
+            (orientationVector.x < 0 && transform.localScale.x * transformMultiplier > 0))
         {
-            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x * -1 * transformMultiplier, transform.localScale.y, transform.localScale.z);
         }
     }
 }
