3d877f41de99bfe29caaf2ec72ce01f37b9c1989
Matouš Kozma <mattkozma@hotmail.com>
2020-03-01 18:41:20 +0100

    Modified the monsters generated to make encounters less deadly for demonstration purposes

--- Assets/Scripts/EncounterGenerator/EncounterGenerator.cs
+++ Assets/Scripts/EncounterGenerator/EncounterGenerator.cs
@@ -15,12 +15,19 @@ public class EncounterGenerator
             TargetEncounterDifficulty = targetDifficulty,
             MonsterPriorities = new Dictionary<GameObject, float>()
         };
-        var testMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Regular, MonsterRole.Brute);
-        var leaderMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Boss, MonsterRole.Lurker);
+        var leaderMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Elite, MonsterRole.Leader);
+        var bruteMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Regular, MonsterRole.Brute);
+        var sniperMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Regular, MonsterRole.Sniper);
+        var minionMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Regular, MonsterRole.Minion);
+        var lurkerMonsterTypeDefinition = new MonsterTypeDefinition(MonsterRank.Regular, MonsterRole.Lurker);
         generationParams.RequestedMonsters = new List<KeyValuePair<MonsterTypeDefinition, int>> {
-            new KeyValuePair<MonsterTypeDefinition, int>(testMonsterTypeDefinition, (int)targetDifficulty-1),
-            new KeyValuePair<MonsterTypeDefinition, int>(leaderMonsterTypeDefinition, 1)
+            new KeyValuePair<MonsterTypeDefinition, int>(bruteMonsterTypeDefinition, (int)targetDifficulty-1),
+        //    new KeyValuePair<MonsterTypeDefinition, int>(leaderMonsterTypeDefinition, 1),
+            new KeyValuePair<MonsterTypeDefinition, int>(sniperMonsterTypeDefinition, 1),
+            new KeyValuePair<MonsterTypeDefinition, int>(minionMonsterTypeDefinition, 4),
+            new KeyValuePair<MonsterTypeDefinition, int>(lurkerMonsterTypeDefinition, 1)
             };
         return configuration.MonsterGroupDefinition.First().GenerateMonsterGroup(generationParams);
     }
+
 }
