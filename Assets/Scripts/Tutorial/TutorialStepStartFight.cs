c1a34a5bb99ca5e0cc38c60a206a659befc9b30e
Matouš Kozma <mattkozma@hotmail.com>
2020-05-01 14:42:13 +0200

    Added tutorial explaining Max HP loss and powerups.

---
+++ Assets/Scripts/Tutorial/TutorialStepStartFight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Combat;
+using Assets.Scripts.DungeonGenerator;
+using Assets.Scripts.Environment;
+using Assets.Scripts.Input;
+
+namespace Assets.Scripts.Tutorial
+{
+    class TutorialStepStartFight : TutorialStepWithMessageBoxBase
+    {
+        private CombatantsManager combatantsManager;
+        protected override void Start()
+        {
+            base.Start();
+            combatantsManager = FindObjectOfType<CombatantsManager>();
+            foreach (var interactableObject in FindObjectsOfType<InteractableObject>())
+            {
+                interactableObject.IsInteractionDisabledByTutorial = false;
+            }
+
+        }
+
+        private void Update()
+        {
+            if (!completedTutorialAction && combatantsManager.IsCombatActive)
+            {
+                messageBox.Hide();
+                completedTutorialAction = true;
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialStepWaitForCombatEnd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Combat;
+
+namespace Assets.Scripts.Tutorial
+{
+    public class TutorialStepWaitForCombatEnd: TutorialStep
+    {
+        private CombatantsManager combatantsManager;
+        private void Start()
+        {
+            combatantsManager = FindObjectOfType<CombatantsManager>();
+        }
+        public override bool IsTutorialStepOver()
+        {
+            return !combatantsManager.IsCombatActive;
+        }
+    }
+}
