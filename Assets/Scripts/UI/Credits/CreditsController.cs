831315812cb703a6e5a259e3d5db4c52df47ba51
Matouš Kozma <mattkozma@hotmail.com>
2020-05-01 21:37:48 +0200

    Fixes to credits.

--- Assets/Scripts/UI/Credits/CreditsController.cs
+++ Assets/Scripts/UI/Credits/CreditsController.cs
@@ -12,6 +12,30 @@ namespace Assets.Scripts.UI.Credits
 {
     public class CreditsController: MonoBehaviour
     {
+        public float scrollSpeed = 10;
+        public GameObject ScrollText;
+        private double animationStart;
+        private void Start()
+        {
+            animationStart = Time.unscaledTime;
+            var canvasTransform = transform as RectTransform;
+            var canvasHeight = canvasTransform.sizeDelta.y;
+            var scrollTransform = ScrollText.transform as RectTransform;
+            scrollTransform.anchoredPosition = new Vector2(scrollTransform.anchoredPosition.x, -canvasHeight);
+        }
+
+        private void Update()
+        {
+            var scrollTransform = ScrollText.transform as RectTransform;
+            var canvasHeight = scrollTransform.sizeDelta.y;
+            var newY = scrollTransform.anchoredPosition.y + Time.deltaTime * scrollSpeed;
+            scrollTransform.anchoredPosition = new Vector2(scrollTransform.anchoredPosition.x, newY);
+            if (canvasHeight < newY)
+            {
+                CreditsOver();
+            }
+        }
+
         public void CreditsOver()
         {
             var analyticsService = FindObjectOfType<AnalyticsService>();
